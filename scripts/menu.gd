59824441858ad27b745126bf666e5fb2cbc42f16
abalaji4 <abalaji4@ncsu.edu>
2025-04-22 01:39:02 -0400

    did i really fix it

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,7 +7,7 @@ func _ready():
 	
 func _on_play_pressed():
 	VideoManager.hide_video()
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
